289544846ccdecbb50b6ef497bea508ce23cbfc5
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-05-11 18:09:07 +0200

    :tada: create the context for theme  :tada:

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
 import Header from './components/Header';
+import { useState } from 'react';
+import {ThemeContext} from "./context/ThemeContext"
 
 function App() {
+  const [theme, setTheme]= useState("light")
   return (
-    <div>
-    <Header/>
-      <Home/>
-    </div>
+    <ThemeContext.Provider value={{theme, setTheme}}>
+      <div className={`${theme==='dark' ? 'bg-[#121212] ':null }`}>
+      <Header/>
+        <Home/>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,42 @@
-import React from 'react'
-import logo from "../assets/images/logo.png"
+import React, { useContext, useEffect, useState } from "react";
+import logo from "../assets/images/logo.png";
 import { IoSearch } from "react-icons/io5";
 import { FaMoon } from "react-icons/fa";
+import { MdSunny } from "react-icons/md";
+import {ThemeContext} from "../context/ThemeContext"
 
 const Header = () => {
+  const [toggle, setToggel] = useState(true);
+  const {theme,setTheme} = useContext(ThemeContext)
+  useEffect(() =>{
+    console.log("Theme", theme);
+  },[])
   return (
-    <div className='flex items-center p-3'>
-        <img src={logo} alt='logo' width={60} height={60}/>
-        <div className='flex bg-slate-200 p-2 w-full items-center mx-5 rounded-full'>
-            <IoSearch/>
-            <input type='text' placeholder='Select Your Video Game' className='bg-transparent px-2 outline-none'/>
-        </div>
-        <div>
-          <FaMoon/>
-        </div>
+    <div className="flex items-center p-3">
+      <img src={logo} alt="logo" width={60} height={60} />
+      <div className="flex bg-slate-200 p-2 w-full items-center mx-5 rounded-full">
+        <IoSearch />
+        <input
+          type="text"
+          placeholder="Select Your Video Game"
+          className="bg-transparent dark:bg-cyan-500 px-2 outline-none"
+        />
+      </div>
+      <div>
+        {theme==="light" ? (
+          <FaMoon
+            className="text-[35px] bg-slate-200 p-1 rounded-full cursor-pointer"
+            onClick={() => setTheme("dark")}
+          />
+        ) : (
+          <MdSunny
+            className="text-[35px] bg-slate-200 p-1 rounded-full cursor-pointer"
+            onClick={() => setTheme("light")}
+          />
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
