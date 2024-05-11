16bf421abc9ab814f342c860906b92495a1a8413
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-05-11 12:46:08 +0200

    :tada: navbar  :tada:

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import logo from "../assets/images/logo.png"
+import { IoSearch } from "react-icons/io5";
+import { FaMoon } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <div>
+    <div className='flex items-center p-3'>
         <img src={logo} alt='logo' width={60} height={60}/>
+        <div className='flex bg-slate-200 p-2 w-full items-center mx-5 rounded-full'>
+            <IoSearch/>
+            <input type='text' placeholder='Select Your Video Game' className='bg-transparent px-2 outline-none'/>
+        </div>
         <div>
-            <input type='text'/>
+          <FaMoon/>
         </div>
     </div>
   )
